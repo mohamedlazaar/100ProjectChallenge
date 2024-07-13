96795a9db71504263ed669f3e4b26a5fad981195
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-07-13 19:41:04 +0100

    Auto_Text_Generator_with_JavaScript

---
+++ SimpleQuiz_JavaScript/quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer() {
+    // Function body
+
+    const correctAnswer= "4";
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;   
+
+    if (userAnswer === correctAnswer){
+        
+        document.getElementById('feedback').textContent = "Correct! Well done.";
+    }else{
+        document.getElementById('feedback').textContent ="That's incorrect. Try again!"
+    }
+}
+
+let btn = document.getElementById('submit-answer');
+btn.addEventListener('click', checkAnswer)
